b2652b2977b4c46487013685b87ff02024b78b5d
andre <adventureandre@hotmail.com>
2022-05-18 21:56:23 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </main>
 
 <footer class="main_footer container">
-    &copy; 2010 - <?= date('Y'); ?> UpInside Treinamentos - Todos Os Direitos Reservados
+    &copy; 2010 - <?= date('Y'); ?> Curso de Design responsivo
 </footer>
 </body>
 </html>
